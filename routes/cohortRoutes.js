3d7fd64787d3d4f95fdc190586a5b09175dacf4a
conconrob <connortprobertson@gmail.com>
2019-03-24 08:54:44 +0000

    done get cohorts endpoint

--- routes/cohortRoutes.js
+++ routes/cohortRoutes.js
@@ -7,4 +7,9 @@ const db = knex(knexConfig.development);
 const routes = express.Router();
 routes.use(express.json());
 
+routes.get("/", async (req, res) => {
+  const cohorts = await db.select("*").from("cohorts");
+  res.status(200).json(cohorts)
+})
+
 module.exports = routes;
