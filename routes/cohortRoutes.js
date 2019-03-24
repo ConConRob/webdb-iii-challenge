6dcdb6bb26cb9c9735f819bfb35db2e74b84fad6
conconrob <connortprobertson@gmail.com>
2019-03-24 09:12:01 +0000

    done get cohorts students endpoint

--- routes/cohortRoutes.js
+++ routes/cohortRoutes.js
@@ -8,25 +8,39 @@ const routes = express.Router();
 routes.use(express.json());
 
 routes.get("/", async (req, res) => {
-  try{
+  try {
     const cohorts = await db.select("*").from("cohorts");
-    res.status(200).json(cohorts)
-  }catch(error){
-    res.status(500).json({message: "Server error", error})
+    res.status(200).json(cohorts);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
   }
-})
+});
 
 routes.get("/:id", async (req, res) => {
-  try{
-    const arrayOfCohort = await db.select("*").from("cohorts").where("id", "=", req.params.id)
-    if(arrayOfCohort.length === 0){
-      res.status(404).json({message: "That cohort does not exist"})
+  try {
+    const arrayOfCohort = await db
+      .select("*")
+      .from("cohorts")
+      .where("id", "=", req.params.id);
+    if (arrayOfCohort.length === 0) {
+      res.status(404).json({ message: "That cohort does not exist" });
     } else {
-      res.status(200).json(arrayOfCohort[0])
+      res.status(200).json(arrayOfCohort[0]);
     }
-  }catch(error) {
-    res.status(500).json({message: "Server error", error})
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
   }
-})
+});
 
+routes.get("/:id/students", async (req, res) => {
+  try{
+    const students = await db
+      .select("*")
+      .from("students")
+      .where("cohort_id","=", req.params.id);
+    res.status(200).json(students)
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error })
+  }
+});
 module.exports = routes;
