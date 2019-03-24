b519192d13f820fa4ee32147f426ef0932bc4e10
conconrob <connortprobertson@gmail.com>
2019-03-24 09:05:48 +0000

    get cohort by id route done

--- routes/cohortRoutes.js
+++ routes/cohortRoutes.js
@@ -8,8 +8,25 @@ const routes = express.Router();
 routes.use(express.json());
 
 routes.get("/", async (req, res) => {
-  const cohorts = await db.select("*").from("cohorts");
-  res.status(200).json(cohorts)
+  try{
+    const cohorts = await db.select("*").from("cohorts");
+    res.status(200).json(cohorts)
+  }catch(error){
+    res.status(500).json({message: "Server error", error})
+  }
+})
+
+routes.get("/:id", async (req, res) => {
+  try{
+    const arrayOfCohort = await db.select("*").from("cohorts").where("id", "=", req.params.id)
+    if(arrayOfCohort.length === 0){
+      res.status(404).json({message: "That cohort does not exist"})
+    } else {
+      res.status(200).json(arrayOfCohort[0])
+    }
+  }catch(error) {
+    res.status(500).json({message: "Server error", error})
+  }
 })
 
 module.exports = routes;
